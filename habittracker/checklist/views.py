d7037007052823d3309a236ce74b3298826c70a4
Sophie <snsophie50@gmail.com>
2026-07-23 15:35:08 -0400

    adding a view and context

--- habittracker/checklist/views.py
+++ habittracker/checklist/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def AddTasks(request):
+    context = {'task': 'read a book'}
+    return render(request, 'checklist/main.html', context)
